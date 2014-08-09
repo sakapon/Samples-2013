d1ff8148b8884dc9f8b75530d345996f40916923
Keiho Sakapon <git@saka-pon.net>
2014-08-09 17:20:47 +0900

    Add DirectoryDb

---
+++ EFSample/DirectoryConsole/DirectoryDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DirectoryConsole
+{
+    public class DirectoryDb : DbContext
+    {
+        public DbSet<Entry> Entries { get; set; }
+    }
+
+    [DebuggerDisplay(@"\{{GetType().Name}: {Name}\}")]
+    public abstract class Entry
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        public Folder Parent { get; set; }
+    }
+
+    public class Folder : Entry
+    {
+        public ICollection<Entry> Subentries { get; set; }
+    }
+
+    public class File : Entry
+    {
+    }
+}
--- EFSample/DirectoryConsole/Program.cs
+++ EFSample/DirectoryConsole/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DirectoryConsole
 {
@@ -10,6 +9,59 @@ namespace DirectoryConsole
     {
         static void Main(string[] args)
         {
+            //InsertData();
+            GetData();
+        }
+
+        static void GetData()
+        {
+            using (var db = new DirectoryDb())
+            {
+                var files = db.Entries
+                    .OfType<File>()
+                    .Include(f => f.Parent)
+                    .ToArray();
+
+                var dir1 = db.Entries
+                    .OfType<Folder>()
+                    .Where(f => f.Name == "Dir 1")
+                    .Include(f => f.Subentries)
+                    .Single();
+
+                foreach (var item in dir1.Subentries)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+
+        static void InsertData()
+        {
+            var root = new Folder
+            {
+                Name = "Root",
+                Subentries = new HashSet<Entry>
+                {
+                    new Folder
+                    {
+                        Name = "Dir 1",
+                        Subentries = new HashSet<Entry>
+                        {
+                            new File { Name = "File 1-1" },
+                            new File { Name = "File 1-2" },
+                        },
+                    },
+                    new Folder { Name = "Dir 2" },
+                    new File { Name = "File 1" },
+                },
+            };
+
+            using (var db = new DirectoryDb())
+            {
+                db.Entries.Add(root);
+
+                db.SaveChanges();
+            }
         }
     }
 }
