7d3b0c7e918da06d0eb1a250c7960b08e699db4f
Keiho Sakapon <git@saka-pon.net>
2014-08-13 23:36:37 +0900

    Change project name

--- CompressionSample/ExcelMediaConsole/Properties/AssemblyInfo.cs
+++ CompressionSample/ExcelMediaConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ExcelImagesConsole")]
+[assembly: AssemblyTitle("ExcelMediaConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExcelImagesConsole")]
+[assembly: AssemblyProduct("ExcelMediaConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
-[assembly: Guid("0bc8021c-3dd2-423a-8e03-94feef97bdcb")]
+[assembly: Guid("06b387f9-27ba-45f5-98f4-f0232aea92be")]
 
 // アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
