e25328e38d1f34badaf7d1360d8bf957dfbd6a9c
Keiho Sakapon <saka_pon@live.jp>
2016-02-20 11:14:58 +0900

    KLibraryJS 1.1.61

---
+++ KLibraryJS/Services/Bakery-Products.js
@@ -0,0 +1,8 @@
+﻿[
+    { "Id": 1, "Name": "Carrot Cake", "Desc": "A scrumptious mini-carrot cake encrusted with sliced almonds", "Price": 8.99, "ImageName": "carrot_cake.jpg", "Company": { "Id": 7, "Name": "Pavlova, Ltd.", "Country": "Australia"} },
+    { "Id": 2, "Name": "Lemon Tart", "Desc": "A delicious lemon tart with fresh meringue cooked to perfection", "Price": 9.99, "ImageName": "lemon_tart.jpg", "Company": { "Id": 8, "Name": "Specialty Biscuits, Ltd.", "Country": "UK"} },
+    { "Id": 3, "Name": "Cupcakes", "Desc": "Delectable vanilla and chocolate cupcakes", "Price": 4.99, "ImageName": "cupcakes.jpg", "Company": { "Id": 7, "Name": "Pavlova, Ltd.", "Country": "Australia"} },
+    { "Id": 4, "Name": "Bread", "Desc": "Fresh baked French-style bread", "Price": 1.49, "ImageName": "bread.jpg", "Company": { "Id": 9, "Name": "PB Knackebrod AB", "Country": "Sweden"} },
+    { "Id": 6, "Name": "Pear Tart", "Desc": "A glazed pear tart topped with sliced almonds and a dash of cinnamon", "Price": 5.99, "ImageName": "pear_tart.jpg", "Company": { "Id": 8, "Name": "Specialty Biscuits, Ltd.", "Country": "UK"} },
+    { "Id": 7, "Name": "Chocolate Cake", "Desc": "Rich chocolate frosting cover this chocolate lover’s dream", "Price": 8.99, "ImageName": "chocolate_cake.jpg", "Company": { "Id": 7, "Name": "Pavlova, Ltd.", "Country": "Australia"} }
+]
